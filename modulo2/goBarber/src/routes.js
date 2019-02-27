acce6e9ad26c1907f3d4e2f9500fb3dfcffac197
Wellington Garcia <wellington@inundaweb.com.br>
2019-02-26 22:12:44 -0300

    Finalizando agendamentos

--- modulo2/goBarber/src/routes.js
+++ modulo2/goBarber/src/routes.js
@@ -11,6 +11,8 @@ const UserControlleer = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController')
 const DashboardController = require('./app/controllers/DashboardController')
 const FileController = require('./app/controllers/FileController')
+const AppointmentController = require('./app/controllers/AppointmentController')
+const AvailableController = require('./app/controllers/AvailableController')
 
 routes.use((req, res, next) => {
   res.locals.flashSucces = req.flash('success')
@@ -30,8 +32,11 @@ routes.post('/signup', upload.single('avatar'), UserControlleer.store)
 routes.use('/app', authMiddleware)
 routes.get('/app/logout', SessionController.destroy)
 
-routes.get('/app/dashboard', DashboardController.index, (req, res) => {
-  return res.render('dashboard')
-})
+routes.get('/app/dashboard', DashboardController.index)
+
+routes.get('/app/appointments/new/:provider', AppointmentController.create)
+routes.post('/app/appointments/new/:provider', AppointmentController.store)
+
+routes.get('/app/available/:provider', AvailableController.index)
 
 module.exports = routes
