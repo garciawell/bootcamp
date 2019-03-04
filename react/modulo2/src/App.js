d76883650827d5c9b746355641e587df00315638
Wellington Garcia <garciawell@gmail.com>
2019-03-04 13:35:22 -0300

    Moudlo 2 react + clear project

---
+++ react/modulo2/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hell WOrdls</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
