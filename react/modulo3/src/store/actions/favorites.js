e60e8c8641ba676426a11f99d4e03174f99446ee
Wellington Garcia <garciawell@gmail.com>
2019-03-05 00:00:26 -0300

    Redux-saga

--- react/modulo3/src/store/actions/favorites.js
+++ react/modulo3/src/store/actions/favorites.js
@@ -1,3 +1,11 @@
-export const addFavorite = () => ({
-  type: 'ADD_FAVORITE',
+export const addFavoriteRequest = repository => ({
+  type: 'ADD_FAVORITE_REQUEST',
+  payload: { repository },
+});
+
+// REQUEST -> SAGA -> CHAMADA API -> SUCCESS
+
+export const addFavoriteSuccess = data => ({
+  type: 'ADD_FAVORITE_SUCCESS',
+  payload: { data },
 });
