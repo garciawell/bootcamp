adcef92fd51428b991de17dd16f2c91cc36355e3
Wellington Garcia <garciawell@gmail.com>
2019-03-05 14:26:05 -0300

    FlexBox Estilo . Editor COnfig

---
+++ react-native/modulo1/src/components/Todo.js
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import { View, Text, StyleSheet } from "react-native";
+
+const Todo = ({ title }) => (
+  <View>
+    <Text>{title}</Text>
+  </View>
+);
+
+Todo.defaultProps = {
+  title: "Todo Padrão"
+};
+
+Todo.propTypes = {
+  title: PropTypes.string.isRequired
+};
+
+const styles = StyleSheet.create({});
+
+export default Todo;
--- react-native/modulo1/src/index.js
+++ react-native/modulo1/src/index.js
@@ -1,17 +1,85 @@
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import { Platform, StyleSheet, Text, View, Button } from "react-native";
+import Todo from "./components/Todo";
 
 export default class App extends Component {
+  state = {
+    counter: 0,
+    text: "",
+    todos: [
+      {
+        id: 0,
+        text: "Fazer Café"
+      },
+      {
+        id: 1,
+        text: "Estudar GO native"
+      }
+    ]
+  };
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        text: "Hello Worlds"
+      });
+    }, 3000);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextState.counter < 5;
+  }
+
+  componentDidUpdate(prevProps, prevState) {}
+
+  componentWillUnmount() {}
+
+  handleAddCounter = () => {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  };
+
+  addTodo = () => {
+    this.setState({
+      todos: [
+        ...this.state.todos,
+        {
+          id: Math.random(),
+          text: "Estudar GO native22"
+        }
+      ]
+    });
+  };
+
   render() {
-    return <View style={styles.container} />;
+    return (
+      <View style={styles.container}>
+        <View style={styles.box} />
+        <View style={styles.box} />
+        <View style={styles.box} />
+        <View style={styles.box} />
+        <View style={styles.box} />
+        <View style={styles.box} />
+      </View>
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#777"
+    flexDirection: "row",
+    backgroundColor: "#333",
+    flexWrap: "wrap",
+    alignContent: "center",
+    justifyContent: "center"
+  },
+  box: {
+    width: 80,
+    height: 80,
+    margin: 10,
+    backgroundColor: "#f00",
+    transform: [{ rotateZ: "20deg" }]
   }
 });
