a13b50d330c4dedb5874b4c3f11d05f4d8daf888
Wellington Garcia <garciawell@gmail.com>
2019-03-06 18:07:11 -0300

    Navegacao Abas

--- react-native/modulo2/src/pages/Organizations/index.js
+++ react-native/modulo2/src/pages/Organizations/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 import { View } from 'react-native';
-
+import Header from '~/components/Header';
 // import styles from './styles';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-const Organizations = () => <View />;
+const Organizations = () => (
+  <View>
+    <Header title="Organização" />
+  </View>
+);
+
+const TabIcon = ({ tintColor }) => <Icon name="building" size={20} color={tintColor} />;
+
+TabIcon.propTypes = {
+  tintColor: PropTypes.string.isRequired,
+};
+
+Organizations.navigationOptions = {
+  tabBarIcon: TabIcon,
+};
 
 export default Organizations;
--- react-native/modulo2/src/pages/Repositories/index.js
+++ react-native/modulo2/src/pages/Repositories/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 import { View } from 'react-native';
 import Header from '~/components/Header';
 // import styles from './styles';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const Repositories = () => (
   <View>
     <Header title="Repositórios" />
   </View>
 );
+
+const TabIcon = ({ tintColor }) => <Icon name="list-alt" size={20} color={tintColor} />;
+
+TabIcon.propTypes = {
+  tintColor: PropTypes.string.isRequired,
+};
+
+Repositories.navigationOptions = {
+  tabBarIcon: TabIcon,
+};
 
 export default Repositories;
