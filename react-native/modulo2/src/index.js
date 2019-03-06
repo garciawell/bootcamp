7157b2f9ce08dbd23aea54e6ae6e6b4df644d3b4
Wellington Garcia <garciawell@gmail.com>
2019-03-06 00:54:55 -0300

    Start Modulo 2

---
+++ react-native/modulo2/src/index.js
@@ -0,0 +1,33 @@
+import './config/ReactotronConfig';
+import './config/DevToolsConfig';
+import React, { Component } from 'react';
+import {
+  StyleSheet, View, Platform, Text,
+} from 'react-native';
+
+console.tron.log('Hello WOrlds');
+
+export default class App extends Component {
+  state = {};
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>APLICATIVO</Text>
+      </View>
+    );
+  }
+}
+
+const bgColor = '#333';
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    backgroundColor: bgColor,
+    flex: 1,
+    flexDirection: 'column',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+});
