6ad0b5daf03933e7c6f2e0e94da9b8e93ef56c2c
Wellington Garcia <garciawell@gmail.com>
2019-03-10 20:52:04 -0300

    Configs Desafio

--- react-native/modulo3/desafio/babel.config.js
+++ react-native/modulo3/desafio/babel.config.js
@@ -1,3 +1,27 @@
-module.exports = {
-  presets: ["module:metro-react-native-babel-preset"]
-}
+module.exports = function (api) {
+  api.cache(true);
+
+  const presets = ['module:metro-react-native-babel-preset'];
+  const plugins = [
+    [
+      'babel-plugin-root-import',
+      {
+        rootPathSuffix: 'src',
+      },
+    ],
+  ];
+
+  if (process.env.ENV === 'prod') {
+    plugins.push([
+      'babel-plugin-root-import',
+      {
+        rootPathSuffix: 'src',
+      },
+    ]);
+  }
+
+  return {
+    presets,
+    plugins,
+  };
+};
