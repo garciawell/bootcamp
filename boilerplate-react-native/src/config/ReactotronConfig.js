2b4ca73487e8e88f6f2ea2a95db0e5b9cf4f0f9c
Wellington Garcia <garciawell@gmail.com>
2019-04-03 23:47:57 -0300

    Folder Boilerplate

---
+++ boilerplate-react-native/src/config/ReactotronConfig.js
@@ -0,0 +1,18 @@
+import Reactotron from "reactotron-react-native";
+import { reactotronRedux } from "reactotron-redux";
+import reactotronSaga from "reactotron-redux-saga";
+
+if (__DEV__) {
+  const tron = Reactotron.configure({
+    // host: '192.168.56.1'
+    host: "10.0.3.2"
+  }) // controls connection & communication settings
+    .useReactNative()
+    .use(reactotronRedux())
+    .use(reactotronSaga())
+    .connect(); // let's connect!
+
+  console.tron = tron;
+
+  tron.clear();
+}
