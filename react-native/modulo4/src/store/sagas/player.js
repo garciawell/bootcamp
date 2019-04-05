c37603bd18187ccbdec6a6c199ab81c11bacf6d6
Wellington Garcia <garciawell@gmail.com>
2019-04-04 23:54:15 -0300

    Atualizando Bolilerplate advanced

--- react-native/modulo4/src/store/sagas/player.js
+++ react-native/modulo4/src/store/sagas/player.js
@@ -2,11 +2,37 @@ import { call, put } from 'redux-saga/effects';
 import TrackPlayer from 'react-native-track-player';
 import PlayerActions from '~/store/ducks/player';
 
+// export function* init() {
+//   yield call(TrackPlayer.setupPlayer);
+
+//   TrackPlayer.addEventListener('playback-track-changed', console.tron.log);
+//   TrackPlayer.addEventListener('playback-state', console.tron.log);
+// }
+
 export function* init() {
   yield call(TrackPlayer.setupPlayer);
 
-  TrackPlayer.addEventListener('playback-track-changed', console.tron.log);
-  TrackPlayer.addEventListener('playback-state', console.tron.log);
+  TrackPlayer.updateOptions({
+    capabilities: [
+      TrackPlayer.CAPABILITY_PLAY,
+      TrackPlayer.CAPABILITY_PAUSE,
+      TrackPlayer.CAPABILITY_SKIP_TO_NEXT,
+      TrackPlayer.CAPABILITY_SKIP_TO_PREVIOUS,
+      TrackPlayer.CAPABILITY_STOP,
+    ],
+    notificationCapabilities: [
+      TrackPlayer.CAPABILITY_PLAY,
+      TrackPlayer.CAPABILITY_PAUSE,
+      TrackPlayer.CAPABILITY_SKIP_TO_NEXT,
+      TrackPlayer.CAPABILITY_SKIP_TO_PREVIOUS,
+      TrackPlayer.CAPABILITY_STOP,
+    ],
+    compactCapabilities: [TrackPlayer.CAPABILITY_PLAY, TrackPlayer.CAPABILITY_PAUSE],
+  });
+
+  TrackPlayer.addEventListener('playback-state', (data) => {
+    console.tron.log('STATE', data);
+  });
 }
 
 export function* setPodcast({ podcast }) {
