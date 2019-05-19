d3ac551d27fd1c15d78abdc1285602a2d66dd2a4
Wellington Garcia <garciawell@gmail.com>
2019-05-19 00:39:37 -0300

    Test

--- node/modulo5/src/app/models/User.js
+++ node/modulo5/src/app/models/User.js
@@ -1,9 +1,27 @@
+const bcrypt = require('bcrypt')
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password_hash: DataTypes.STRING
-  })
+  const User = sequelize.define(
+    'User',
+    {
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.VIRTUAL,
+      password_hash: DataTypes.STRING
+    },
+    {
+      hooks: {
+        beforeSave: async user => {
+          if (user.password) {
+            user.password_hash = await bcrypt.hash(user.password, 8)
+          }
+        }
+      }
+    }
+  )
+
+  User.prototype.checkPassword = function (password) {
+    return bcrypt.compare(password, this.password_hash)
+  }
 
   return User
 }
