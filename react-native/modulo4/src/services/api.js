3e9073558c65735809a83002633aa8a8da8b2247
Wellington Garcia <garciawell@gmail.com>
2020-02-18 11:46:58 -0300

    api heroku spotify

--- react-native/modulo4/src/services/api.js
+++ react-native/modulo4/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://10.0.3.2:3000',
+  // baseURL: 'http://10.0.3.2:3000',
+  baseURL: 'https://api-spotify-ng.herokuapp.com',
 });
 
 export default api;
